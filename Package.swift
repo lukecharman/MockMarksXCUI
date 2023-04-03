08bedaa340558a98a5c422fb50892b14419fb14e
Luke Charman <Luke.Charman@mnscorp.netLuke.Charman@mnscorp.netLuke.Charman@mnscorp.net>
2023-04-03 16:09:10 +0100

    Remove Swiftlint dep for now

--- Package.swift
+++ Package.swift
@@ -11,14 +11,12 @@ let package = Package(
         ),
     ],
     dependencies: [
-      .package(url: "https://github.com/Realm/SwiftLint", branch: "main"),
       .package(url: "https://github.com/lukecharman/MockMarks", branch: "main")
     ],
     targets: [
         .target(
             name: "MockMarks+XCUI",
-            dependencies: ["MockMarks"],
-            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+            dependencies: ["MockMarks"]
         )
     ]
 )
